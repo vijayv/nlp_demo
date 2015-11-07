0b3c2b6d3af3ab63eb28a175f5a2d9c493a7b323
Vijay Velagapudi <acchi369@gmail.com>
2015-11-06 16:16:43 -0800

    update to use little bit react

--- __init__.py
+++ __init__.py
@@ -1,9 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, Response
+from bson import json_util
 import pymongo
+import datetime
 app = Flask(__name__)
 
 mongoClient = pymongo.MongoClient('ds031883.mongolab.com',
-								 port=31883,
+    					         port=31883,
 			                     connectTimeoutMS=30000,
 			                     socketTimeoutMS=None,
 			                     socketKeepAlive=True)
@@ -18,10 +20,20 @@ def home():
     return render_template('index.html')
 
 
-@app.route("/ajax")
-def ajax():
-    l = db.activeTrackDirectory.find_one()
-    return render_template('index.html', collections=l)
+@app.route("/api/v1/tracks")
+def apiv1tracks():
+    start_date = datetime.datetime(2015, 11, 4, 2, 00, 0, 000)
+    l = [each for each in db.activeTrackDirectory.find({"trackGenre": "Hard"}).limit(10)]
+    return Response(
+        json_util.dumps(l),
+        mimetype="application/json")
+
+@app.route("/api/v1/genres")
+def apiv1genres():
+    l = [each for each in db.activeTrackDirectory.find().distinct("trackGenre")]
+    return Response(
+        json_util.dumps(l),
+        mimetype="application/json")
 
 
 if __name__ == "__main__":
